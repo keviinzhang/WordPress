bd041fa1b14aed1aa75a897587f4e338437657d1
keviinzhang <kevinzhang_96@yahoo.se>
2019-03-06 14:00:02 +0100

    Initial commit

---
+++ untitled.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+</head>
+<body>
+<h1>hej</h1>
+<?php
+print phpinfo()
+?>
+</body>
+</html>
+<?php
+
+/*
+Plugin Name: Untitled
+Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
+Description: A brief description of the Plugin.
+Version: 1.0
+Author: kanzh
+Author URI: http://URI_Of_The_Plugin_Author
+License: A "Slug" license name e.g. GPL2
+*/
